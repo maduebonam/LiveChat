2a892b2512ee646b8d4ea260a103dad9e83e8967
kingsley <kingsleymaduebonam@gmail.com>
2024-11-26 14:27:33 +0100

    Fix: Add path module import in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import path from 'path';
+
 
 export default defineConfig({
   plugins: [
